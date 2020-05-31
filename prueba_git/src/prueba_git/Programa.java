e2554fa91a99442d66f4d1d571a473984f5d35cf
ivacave <ivancap@alumnos.iesgalileo.es>
2020-05-31 21:49:11 +0200

    primera version de la ventana

---
+++ prueba_git/src/prueba_git/Programa.java
@@ -0,0 +1,16 @@
+package prueba_git;
+
+/**
+ *
+ * @author Iván Cavero Prieto
+ */
+public class Programa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
